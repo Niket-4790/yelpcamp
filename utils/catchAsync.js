b756084c75076ca91960aeea1f0cbccdc3b97856
Niket-4790 <158852075+Niket-4790@users.noreply.github.com>
2024-09-03 00:43:25 +0530

    Update catchAsync.js

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -3,3 +3,6 @@ module.exports=func=>{
         func(req,res,next).catch(next);
     }
 }
+//This code provides a convenient way to handle errors in asynchronous Express route handlers.
+//By wrapping route handlers with this function, you ensure that any errors that occur in those handlers are automatically passed to the Express error-handling middleware.
+//This reduces the need for repetitive try-catch blocks in your asynchronous route handlers, leading to cleaner and more maintainable code.
