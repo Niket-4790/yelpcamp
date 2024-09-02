01075af8577e2da6c9ec0a2b2a30085dc74b7cc9
Niket-4790 <158852075+Niket-4790@users.noreply.github.com>
2024-09-03 00:36:11 +0530

    Update ExpressError.js

--- utils/ExpressError.js
+++ utils/ExpressError.js
@@ -6,3 +6,5 @@ class ExpressError extends Error{
     }
 }
 module.exports=ExpressError;
+//This code defines a custom error class called ExpressError that extends the built-in Error class in JavaScript.
+//this class aims to create more specific and meaningful error objects that can be used in an Express.js application.
